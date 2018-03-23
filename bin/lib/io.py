6223ad9f21c232e72760d4dde772fe3527788778
Roope Tervo <roope.tervo@fmi.fi>
2018-03-23 12:59:43 +0200

    Modifications required to ran in dataproc

--- bin/lib/io.py
+++ bin/lib/io.py
@@ -8,16 +8,17 @@ from keras.models import Model, model_from_yaml
 import boto3
 import random
 import pickle
-
+import json
 
 class IO:
 
     s3 = False
+    gs = False
     bucket_name = ''
     bucket = ''
     client = ''
     
-    def __init__(self, s3_bucket=False):
+    def __init__(self, s3_bucket=False, gs_bucket=False):
 
         if s3_bucket != False:
             self.bucket_name = s3_bucket
@@ -25,89 +26,25 @@ class IO:
             self.client = boto3.client('s3')
             resource = boto3.resource('s3')
             self.bucket = resource.Bucket(self.bucket_name)
+        elif gs_bucket != False:
+            self.bucket_name = gs_bucket
+            self.gs = True
+            # TODO continue
     
 
-    def save_model(self, model_filename, weights_filename, history_filename, model, history):
-        """ Save model and weights into file """
-        print("Saving model into {} and weights into {}".format(model_filename, weights_filename))
-        with open(model_filename, 'w') as f:
-            f.write(model.to_yaml())
-            model.save_weights(weights_filename)
 
-        with open(history_filename, 'wb') as f:
-            pickle.dump(history, f)
-
-        if self.s3:
-            self.bucket.upload_file(model_filename, model_filename)
-            self.bucket.upload_file(weights_filename, weights_filename)
-            self.bucket.upload_file(history_filename, history_filename)
-
-
-    def load_model(self, model_filename, weights_filename, history_filename, force_local=False):
-        """ Load model and weights from file """
-
-        model_file_to_open = model_filename
-        weights_file_to_open = weights_filename
-        history_file_to_open = history_filename
-        if self.s3 and not force_local:
-            tmp_m = tempfile.NamedTemporaryFile()
-            self.bucket.download_file(model_filename, tmp_m.name)
-            model_file_to_open=tmp_m.name
-
-            tmp_w = tempfile.NamedTemporaryFile()
-            self.bucket.download_file(weights_filename, tmp_w.name)
-            weights_file_to_open = tmp_w.name
-
-            tmp_h = tempfile.NamedTemporaryFile()
-            self.bucket.download_file(history_filename, tmp_h.name)
-            history_file_to_open = tmp_h.name
             
-        with open(model_file_to_open, 'r') as f:
-            model = model_from_yaml(f.read())            
-        model.load_weights(weights_file_to_open)
-        # self.pred_fun = get_layer_output_function(model, 'output_realtime')
-        print('Model loaded')
 
-        with open(history_file_to_open, 'rb') as f:
-            history = pickle.load(f)
-        
-        return model, history
-        
-    def write_probabilities(self, filename, ids, data):
-        """ Write results to file """
-
-        file = open(filename, 'w')
-        file.write("Sample_id,Class_1,Class_2,Class_3,Class_4,Class_5,Class_6,Class_7,Class_8,Class_9,Class_10\n")
-        i = 0
-        for line in data:
-            l = [ '%.4f' % elem for elem in line ]
-            file.write(str(ids[i]) + ',' + ','.join(l)+'\n')
-            i += 1
-            
-        file.close()
-
-    def add_dim(self, x, value=1):
-        new = []
-        for row in x:
-            new_row = []
-            for i in x:
-                new_row.append([i,value])
-            new.append(new_row)
-        return new
-
-    def shuffle(self, X, y):
-        print("Shuffling data...")
-        c = list(zip(X, y))
-        random.shuffle(c)
-        X, y = zip(*c)
-        X, y = list(X), list(y)
-        return X, y
-
-    
+    #
+    # GENERAL
+    #
     def read_data(self, xfilename, yfilename=None,
-                  data_type=float, delimiter=';',
-                  skip_cols=0, skip_rows=1, force_local=False):
-        """ Read data from files """
+                  data_type=None, delimiter=';',
+                  skip_cols=0, skip_rows=1,
+                  force_local=False, remove=None):
+        """ 
+        Read data from csv files
+        """
         X, y = [], []
         
         # Read data    
@@ -117,12 +54,20 @@ class IO:
             with open(tmp.name) as f:                
                 lines = f.read().splitlines()
         else:
-            with open(xfilename) as f:
+            with open(xfilename, encoding='utf-8') as f:
                 lines = f.read().splitlines()
             
         for line in lines[skip_rows:]:
             l = line.split(delimiter)[skip_cols:]
-            X.append(list(map(data_type, l)))
+            if remove is not None:
+                nl = []
+                for e in l:
+                    nl.append(e.replace(remove, ''))
+                l = nl
+                if data_type is not None:
+                    l = list(map(data_type, l))
+            
+            X.append(l)
         
         try:
             if self.s3 and not force_local:
@@ -141,37 +86,6 @@ class IO:
         
         return X, y
     
-    def split_data(self, x, y, num_of_batches, num_of_samples=None):
-        """ Split data to train and validation data set 
-        x:               [matrix, n,k]  feature list (samples as rows, features as cols)
-        y:               [matrix, n,1]  label list
-        num_of_batches:  [int]        number of batches to make
-        num_of_samples:  [int]        number of samplest to pick to every batch (if None, all are taken)
-        """
-
-        selected, ids, batches_x, batches_y = [], [], [], []
-        r = random.SystemRandom()
-        k = len(x)
-        batch_size = k/num_of_batches
-        if num_of_samples is not None:
-            if batch_size > num_of_samples:
-                batch_size = num_of_samples
-                
-        for batch_num in range(num_of_batches):
-            batch_x, batch_y, batch_ids = [], [], []
-        
-            while len(batch_x) < batch_size and len(x) > 0:
-                i = r.randrange(0, len(x))            
-                batch_x.append(x.pop(i))
-                batch_y.append(y.pop(i))
-                batch_ids.append(i)
-            
-            batches_x.append(np.matrix(batch_x))
-            batches_y.append(np.matrix(batch_y).T)
-            ids.append(batch_ids)
-            
-        return ids, batches_x, batches_y
-
     def pick_set(self, x, y, num_of_samples):
         """ Split data to train and validation data set 
         x:               [list, n,k]  feature list (samples as rows, features as cols)
@@ -202,20 +116,6 @@ class IO:
             new_v.append(i+shift)
         return new_v
 
-    def get_classes(self, X, thresholds=[0, 0.2, 0.4, 0.6]):
-        """
-        Get classes from X based on thresholds.
-        """
-        classes = []
-        for x in X:
-            i=len(thresholds) - 1
-            _class = i
-            while i >= 0:
-                if x <= thresholds[i]:
-                    _class = i
-                i -= 1
-            classes.append(_class)
-        return classes                
 
     def read_parameters(self, filename, force_local=False):
         """
@@ -235,3 +135,109 @@ class IO:
             lines = f.read().splitlines()
 
         return lines
+
+    def save_model(self, model_filename, weights_filename, history_filename, model, history):
+        """ 
+        Save keras model and weights into file 
+        """
+        print("Saving model into {} and weights into {}".format(model_filename, weights_filename))
+        with open(model_filename, 'w') as f:
+            f.write(model.to_yaml())
+            model.save_weights(weights_filename)
+
+        with open(history_filename, 'wb') as f:
+            pickle.dump(history, f)
+
+        if self.s3:
+            self.bucket.upload_file(model_filename, model_filename)
+            self.bucket.upload_file(weights_filename, weights_filename)
+            self.bucket.upload_file(history_filename, history_filename)
+
+
+    def load_model(self, model_filename, weights_filename, history_filename, force_local=False):
+        """ 
+        Load keras model and weights from file 
+        """
+
+        model_file_to_open = model_filename
+        weights_file_to_open = weights_filename
+        history_file_to_open = history_filename
+        if self.s3 and not force_local:
+            tmp_m = tempfile.NamedTemporaryFile()
+            self.bucket.download_file(model_filename, tmp_m.name)
+            model_file_to_open=tmp_m.name
+
+            tmp_w = tempfile.NamedTemporaryFile()
+            self.bucket.download_file(weights_filename, tmp_w.name)
+            weights_file_to_open = tmp_w.name
+
+            tmp_h = tempfile.NamedTemporaryFile()
+            self.bucket.download_file(history_filename, tmp_h.name)
+            history_file_to_open = tmp_h.name
+            
+        with open(model_file_to_open, 'r') as f:
+            model = model_from_yaml(f.read())            
+        model.load_weights(weights_file_to_open)
+        # self.pred_fun = get_layer_output_function(model, 'output_realtime')
+        print('Model loaded')
+
+        with open(history_file_to_open, 'rb') as f:
+            history = pickle.load(f)
+        
+        return model, history
+
+
+    
+    #
+    # TRAINS
+    #
+    def get_train_stations(self, filename=None):
+        """
+        Get railway stations from file or db digitrafic
+        """
+        if filename is None:
+            url = "https://rata.digitraffic.fi/api/v1/metadata/stations"
+
+            with urllib.request.urlopen(url) as u:
+                data = json.loads(u.read().decode("utf-8"))
+        else:
+            with open(filename) as f:
+                data = json.load(f)
+
+        stations = dict()
+       
+        for s in data:
+            latlon = {'lat': s['latitude'], 'lon': s['longitude']}
+            stations[s['stationShortCode'].encode('utf-8').decode()] = latlon
+
+        return stations
+
+    def find_id(self, locations, name):
+        """
+        Find id from (id, name) tuple list
+        """
+        for loc in locations:
+            if name == loc[1]:
+                return loc[0]
+
+        return None    
+    
+
+    #
+    # SASSE
+    #     
+    def get_classes(self, X, thresholds=[0, 0.2, 0.4, 0.6]):
+        """
+        Get classes from X based on thresholds.
+        """
+        classes = []
+        for x in X:
+            i=len(thresholds) - 1
+            _class = i
+            while i >= 0:
+                if x <= thresholds[i]:
+                    _class = i
+                i -= 1
+            classes.append(_class)
+        return classes                
+
