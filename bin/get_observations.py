f1f7d4cea8cbb6147f3797c02ae888974ff7a0dd
Roope Tervo <roope.tervo@fmi.fi>
2018-04-04 10:15:15 +0300

    Added multiprocessing

--- bin/get_observations.py
+++ bin/get_observations.py
@@ -10,6 +10,7 @@ import numpy as np
 import urllib.request, json
 import requests
 import codecs
+import multiprocessing
 
 from mlfdb import mlfdb
 import lib.io
@@ -20,38 +21,43 @@ def pointtime_in_metadata(metadata, location_id, time):
             return True
     return False
 
-def main():
+def split_timerange(starttime, endtime, days=1, hours=0):
     """
-    Get observations near locations from SmartMet Server
+    Split timerange to n days
     
-    Data start and end time and timestep is fetched from the
-    data. Dataset is assumed coherent in means of time and
-    locations. I.e. timestep is assumed to be constant between start
-    and end time. 
+    starttime : Datetime
+                starttime
+    endtime : Datetime
+              endtime
+    days : int
+           time chunk length, days
+    hours : int
+           time chunk length, hours
+
+    return list of tuples [(starttime:Datetime, endtime:Datetime)]
     """
-    a = mlfdb.mlfdb()
-    io = lib.io.IO()    
+    chunks = []
+    start = starttime
+    end = start + timedelta(days=days, hours=hours)
+    while end <= endtime:        
+        chunks.append((start, end))
+        start = end
+        end = start + timedelta(days=days, hours=hours)
+    chunks.append((start, end))
+    return chunks
 
-    params = io.read_parameters('cnf/parameters.txt')
+def process_timerange(latlons, starttime, endtime, timestep, params, producer, ids):
+    """
+    Process timerange
+    """
 
-    # Get locations and create coordinate list for SmartMet
-    latlons = []
-    ids = dict()
-    if options.locations is not None:
-        locations = options.locations.split(';')
-        for loc in locations:
-            id, latlon = loc.split(':')
-            latlons.append(latlon)
-            ids[latlon] = id
-    else:
-        locations = a.get_locations_by_dataset(options.dataset)
-        for loc in locations:
-            latlon = str(loc[3])+','+str(loc[2])
-            latlons.append(latlon)
-            ids[latlon] = loc[0]
+    startstr = starttime.strftime('%Y-%m-%dT%H:%M:%S')
+    endstr = endtime.strftime('%Y-%m-%dT%H:%M:%S')
+    logging.info('Processing time {} - {}...'.format(startstr, endstr))
 
+    a = mlfdb.mlfdb(options.db_config_file)
     # Create url and get data
-    url = 'http://data.fmi.fi/fmi-apikey/9fdf9977-5d8f-4a1f-9800-d80a007579c9/timeseries?format=json&producer={producer}&timeformat=epoch&latlons={latlons}&timestep={timestep}&starttime={starttime}&endtime={endtime}&param={params}'.format(latlons=','.join(latlons), timestep=options.timestep, params=','.join(params), starttime=options.starttime, endtime=options.endtime, producer=options.producer)
+    url = 'http://data.fmi.fi/fmi-apikey/9fdf9977-5d8f-4a1f-9800-d80a007579c9/timeseries?format=json&producer={producer}&tz=local&timeformat=epoch&latlons={latlons}&timestep={timestep}&starttime={starttime}&endtime={endtime}&param={params}'.format(latlons=','.join(latlons), timestep=options.timestep, params=','.join(params), starttime=startstr, endtime=endstr, producer=options.producer)
 
     logging.info('Loading data from SmartMet Server...')
     logging.debug('Using url: {}'.format(url))
@@ -60,7 +66,7 @@ def main():
         data = json.loads(u.read().decode("utf-8"))
         
     # Parse data to numpy array
-    logging.info('Parsing data to np array...')
+    logging.debug('Parsing data to np array...')
     result = []
     for el in data:
         row = []
@@ -91,7 +97,6 @@ def main():
     count = 0
     
     # Masks don't work because result and labels are not necessary in the same order
-
     #lm = np.array(labels_metadat)
     #mask = np.ones_like(result)*(places[:]==result[:,1])
     #result_time = np.ma.MaskedArray(result, mask)
@@ -124,10 +129,61 @@ def main():
     logging.debug('Inserting new dataset to db...')
     a.add_rows('feature', header, data, metadata, options.dataset)
 
+    return len(data)
+
+def main():
+    """
+    Get observations near locations from SmartMet Server
+    
+    Data start and end time and timestep is fetched from the
+    data. Dataset is assumed coherent in means of time and
+    locations. I.e. timestep is assumed to be constant between start
+    and end time. 
+    """
+
+    # Initialize process pool
+    job_count = options.job_count
+    if job_count < 0:
+        job_count = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(job_count)
+
+    io = lib.io.IO()    
+    params = io.read_parameters('cnf/parameters.txt')
+    
+    # Get locations and create coordinate list for SmartMet
+    a = mlfdb.mlfdb(options.db_config_file)
+    latlons = []
+    ids = dict()
+    if options.locations is not None:
+        locations = options.locations.split(';')
+        for loc in locations:
+            id, latlon = loc.split(':')
+            latlons.append(latlon)
+            ids[latlon] = id
+    else:
+        locations = a.get_locations_by_dataset(options.dataset)
+        for loc in locations:
+            latlon = str(loc[3])+','+str(loc[2])
+            latlons.append(latlon)
+            ids[latlon] = loc[0]
+
+
+    # Split time range to chunks and process them in paraller
+    start = datetime.strptime(options.starttime, '%Y-%m-%d')
+    end = datetime.strptime(options.endtime, '%Y-%m-%d')
+
+    res = [pool.apply_async(process_timerange, args=(latlons, chunk[0], chunk[1], options.timestep, params, options.producer, ids)) for chunk in split_timerange(start, end, days=0, hours=6)]
+    res = [p.get() for p in res]
+
+    logging.info('Added {} rows observations to db.'.format(sum(res)))
     
 if __name__=='__main__':
 
     parser = argparse.ArgumentParser()
+    parser.add_argument('--job_count',
+                        type=int,
+                        default=1,
+                        help='Job count for processing. Use -1 for using all cores')
     parser.add_argument('--dataset',
                         type=str,
                         default=None,
@@ -163,6 +219,11 @@ if __name__=='__main__':
                         type=str,
                         default='INFO',
                         help='options: DEBUG,INFO,WARNING,ERROR,CRITICAL')
+    parser.add_argument('--db_config_file',
+                        type=str,
+                        default=None,
+                        help='GS address for db config file (if none, ~/.mlfdbconfig is used)')
+
 
     options = parser.parse_args()
     
