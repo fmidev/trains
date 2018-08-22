8469008ff6ea5670b32f8bda418b4b6047fdbef9
Roope Tervo <roope.tervo@fmi.fi>
2018-08-22 09:18:49 +0300

    Moved some helper functions to separate class so that predictions can be run without huge amount of libraries

---
+++ run_in_gcloud.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FILE=$1
+NAME=$2
+CONT_NAME=${NAME//[_:]/-}
+
+docker build -t eu.gcr.io/trains-197305/$NAME -f $FILE .
+gcloud docker -- push eu.gcr.io/trains-197305/$NAME
+gcloud beta compute instances create-with-container $CONT_NAME --container-image eu.gcr.io/trains-197305/$NAME --custom-cpu 32 --custom-memory 64 --boot-disk-size 200 --preemptible --container-restart-policy never
