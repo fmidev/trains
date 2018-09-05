bf7274ff7866185bf27c724e02fadab2197b15aa
Roope Tervo <roope.tervo@fmi.fi>
2018-09-05 15:11:43 +0200

    Increased loss pow to 3

--- bin/lib/LSTM.py
+++ bin/lib/LSTM.py
@@ -66,7 +66,8 @@ class LSTM(object):
         with tf.name_scope('loss'):
             reshape_target = tf.reshape(self.y, [-1], name='reshape_target')
             reshape_pred = tf.reshape(self.pred, [-1], name='reshape_pred')
-            self.loss = tf.square(tf.reduce_mean(tf.squared_difference(reshape_target, reshape_pred)))
+            # self.loss = tf.square(tf.reduce_mean(tf.squared_difference(reshape_target, reshape_pred)))
+            self.loss = tf.pow(tf.reduce_mean(tf.squared_difference(reshape_target, reshape_pred)), 3)
             self.rmse = tf.sqrt(tf.reduce_mean(tf.squared_difference(reshape_target, reshape_pred)))
             self.mse = tf.square(tf.reduce_mean(tf.squared_difference(reshape_target, reshape_pred)))
             self.mae = tf.reduce_mean(tf.losses.absolute_difference(reshape_target, reshape_pred))
