554d025850ae5146034991425053a11425a4ab96
Roope Tervo <roope.tervo@fmi.fi>
2018-09-07 12:32:10 +0200

    some work done

--- run_in_gcloud.sh
+++ run_in_gcloud.sh
@@ -6,4 +6,4 @@ CONT_NAME=${NAME//[_:]/-}
 
 docker build -t eu.gcr.io/trains-197305/$NAME -f $FILE .
 gcloud docker -- push eu.gcr.io/trains-197305/$NAME
-gcloud beta compute instances create-with-container $CONT_NAME --container-image eu.gcr.io/trains-197305/$NAME --custom-cpu 32 --custom-memory 64 --boot-disk-size 200 --preemptible --container-restart-policy never --tags http-server
+gcloud beta compute instances create-with-container $CONT_NAME --container-image eu.gcr.io/trains-197305/$NAME --custom-cpu 16 --custom-memory 32 --boot-disk-size 200 --preemptible --container-restart-policy never --tags http-server
